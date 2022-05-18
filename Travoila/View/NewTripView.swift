34124837facd166a0a7eb142b6c795991a53c272
Patrick Louis <patrick.louis898@gmail.com>
2022-05-18 21:01:57 +0700

    Home Screen #3

--- Travoila/View/NewTripView.swift
+++ Travoila/View/NewTripView.swift
@@ -1,20 +1,80 @@
 //
-//  NewTripView.swift
+//  Newtrip.swift
 //  Travoila
 //
-//  Created by Patrick Louis on 18/05/22.
+//  Created by Patrick Louis on 15/05/22.
 //
 
 import SwiftUI
 
 struct NewTripView: View {
+    
+    @State var tripTitle: String = ""
+    @State var tripDestination: String = ""
+    
+    @State var startDate: Date = Date()
+    @State var endDate: Date = Date()
+    
+    @State var totalBudgetEstimation: String = ""
+    
+    @Binding var trips: [Trip]
+    @Binding var isNoTrip: Bool
+    
+    @Environment(\.presentationMode) var mode: Binding<PresentationMode>
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Form {
+                Section ( header: Text("Trip Title")) {
+                    TextField("Trip Title", text: $tripTitle)
+                }
+
+                Section ( header: Text("Trip Destination")) {
+                    TextField("Trip Destination", text: $tripDestination)
+                }
+
+                Section(header: Text("Date")){
+                    DatePicker("Start Date", selection: $startDate, displayedComponents: .date)
+                    DatePicker("End Date", selection: $endDate, displayedComponents: .date)
+                }
+
+                Section(header: Text("Total Budget Estimation")){
+                    TextField("IDR 0",text: $totalBudgetEstimation)
+                }
+            }
+
+            Button(action: {
+                createNewTrip()
+            }){
+                Text("Save")
+                    .bold()
+                    .frame(width: 340, height: 50)
+                    .background(Color("CustomColor"))
+                    .foregroundColor(.white)
+                    .cornerRadius(10.0)
+            }
+            .frame(minWidth:0,  maxWidth: .infinity)
+        }
+        .background(Color.gray.opacity(0.1))
+    }
+    
+    func createNewTrip(){
+        trips.append(
+            Trip(title: tripTitle, destination: tripDestination, startDate: startDate, endDate: endDate, totalBudgetEstimation: Int(totalBudgetEstimation) ?? 0, categories: [] )
+        )
+        
+        isNoTrip = false
+        
+        mode.wrappedValue.dismiss()
     }
 }
 
 struct NewTripView_Previews: PreviewProvider {
+    
+    @State private static var dummyData: [Trip] = []
+    @State private static var isNoTrip: Bool = true
+    
     static var previews: some View {
-        NewTripView()
+        NewTripView(trips: $dummyData, isNoTrip: $isNoTrip)
     }
 }
