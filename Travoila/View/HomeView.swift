6a2dff2a1ed7f6c75725c142a940c25f10782721
Patrick Louis <patrick.louis898@gmail.com>
2022-05-18 20:03:49 +0700

    Home Screen #3

--- Travoila/View/HomeView.swift
+++ Travoila/View/HomeView.swift
@@ -9,119 +9,194 @@ import SwiftUI
 
 struct HomeView: View {
     
-    @State private var AddNewExpenses = 0
+//    @State private var AddNewExpenses = 0
+    @State var isNoTrip: Bool = true
+    @State var trips: [Trip] = []
     
     var body: some View {
-//        VStack {
-//            ZStack (alignment: .top) {
-//                Rectangle()
-//                    .frame(width: .infinity, height: 120)
-//                    .foregroundColor(Color.gray.opacity(0.1))
-//                    .ignoresSafeArea()
-//
-//                HStack {
-//                    Text("Budget Trips")
-//                        .frame(width: 170, height: 30, alignment: .leading)
-//                        .font(.system(size: 28, weight: .bold))
-//
-//                    Spacer()
-//
-//                    Image(systemName: "plus")
-//                        .font(.title)
-//                        .foregroundColor(Color("CustomColor"))
+        
+//        VStack (spacing: isNoTrip ? 200 : 0) {
+//                    ZStack (alignment: .top) {
+//                        Rectangle()
+//                            .frame(width: .infinity, height: 120)
+//                            .foregroundColor(Color.gray.opacity(0.1))
+//                            .ignoresSafeArea()
+//                        
+//                        HStack {
+//                            Text("Budget Trips")
+//                                .frame(width: 170, height: 30, alignment: .leading)
+//                                .font(.system(size: 28, weight: .bold))
+//                            
+//                            Spacer()
+//                            
+//                            Image(systemName: "plus")
+//                                .font(.title)
+//                                .foregroundColor(Color("CustomColor"))
+//                        }
+//                        .padding(.all)
+//                    }
+//                    
+//                    ScrollView {
+//                        
+//                        if(isNoTrip){
+//                            Text("No Budget Trip Plan")
+//                                .frame(width: .infinity)
+//                                .font(.system(size: 20,weight: .medium))
+//                            
+//                            Spacer()
+//                                .frame(height: 10)
+//                            
+//                            Text("You have not created any budget trip expenses yet")
+//                                .multilineTextAlignment(.center)
+//                                .frame(width: 280, height: 50)
+//                                .font(.system(size: 16,weight: .thin))
+//                            
+//                            Spacer()
+//                                .frame(height: 30)
+//                        } else {
+//                            Text("Sudah ada data")
+//                        }
+//                        
+//                        
+//                        NavigationLink(destination: Newtrip(trips: $trips, isNoTrip: $isNoTrip)){
+//                            Text("Create Budget Trip")
+//                                .bold()
+//                                .padding(.horizontal, 30.0)
+//                                .padding(.vertical, 20.0)
+//                                .background(Color("CustomColor"))
+//                                .foregroundColor(.white)
+//                                .cornerRadius(10.0)
+//                        }
+//                        
+//        //                Button(action: {
+//        //
+//        //                }) { Section {
+//        //                    Text("Create Budget Trip")
+//        //                        .foregroundColor(Color.white)
+//        //                        .padding(.horizontal)
+//        //                        .padding(.vertical, 15.0)
+//        //                        .frame(minWidth:0,  maxWidth: .infinity, alignment: .top)
+//        //                        .background(
+//        //                            RoundedRectangle(cornerRadius: 10)
+//        //                                .stroke(lineWidth: 0)
+//        //                                .background(Color("CustomColor").cornerRadius(10)))
+//        //                }
+//        //                .frame(minWidth:0, maxWidth: 200, alignment: .top)
+//        //                }
+//                    }
+//                    
 //                }
-//                .padding(.all)
+//                .offset(y: -40.0)
+////        VStack {
+////            ZStack (alignment: .top) {
+////                Rectangle()
+////                    .frame(width: .infinity, height: 120)
+////                    .foregroundColor(Color.gray.opacity(0.1))
+////                    .ignoresSafeArea()
+////
+////                HStack {
+////                    Text("Budget Trips")
+////                        .frame(width: 170, height: 30, alignment: .leading)
+////                        .font(.system(size: 28, weight: .bold))
+////
+////                    Spacer()
+////
+////                    Image(systemName: "plus")
+////                        .font(.title)
+////                        .foregroundColor(Color("CustomColor"))
+////                }
+////                .padding(.all)
+////            }
+////
+////            ScrollView {
+////
+////                Text("No Budget Trip Plan")
+////                    .frame(width: .infinity)
+////                    .font(.system(size: 20,weight: .medium))
+////
+////                Spacer()
+////                    .frame(height: 10)
+////
+////                Text("You have not created any budget trip expenses yet")
+////                    .multilineTextAlignment(.center)
+////                    .frame(width: 280, height: 50)
+////                    .font(.system(size: 16,weight: .thin))
+////
+////                Button(action: {
+////
+////                }) { Section {
+////                    Text("Create Budget Trip")
+////                        .foregroundColor(Color.white)
+////                        .padding(.horizontal)
+////                        .padding(.vertical, 15.0)
+////                        .frame(minWidth:0,  maxWidth: .infinity, alignment: .top)
+////                        .background(
+////                            RoundedRectangle(cornerRadius: 10)
+////                                .stroke(lineWidth: 0)
+////                                .background(Color("CustomColor").cornerRadius(10)))
+////                }
+////                .frame(minWidth:0, maxWidth: 200, alignment: .top)
+////                }
+////            }
+////
+////        }
+////
+////    }
+//        NavigationView {
+//            TabView(selection: .constant(1)) {
+//                Text("INI SATU").tabItem {
+//                    Image(systemName: "house")
+//                    Text("Home") }.tag(1)
+//
+//                Text("INI DUA").tabItem {
+//                    Image(systemName: "airplane.circle")
+//                    Text("My Trips") }.tag(2)
 //            }
 //
-//            ScrollView {
+//            .navigationTitle("Budget Trip")
 //
-//                Text("No Budget Trip Plan")
-//                    .frame(width: .infinity)
-//                    .font(.system(size: 20,weight: .medium))
+//            Button (action: {
 //
-//                Spacer()
-//                    .frame(height: 10)
-//
-//                Text("You have not created any budget trip expenses yet")
-//                    .multilineTextAlignment(.center)
-//                    .frame(width: 280, height: 50)
-//                    .font(.system(size: 16,weight: .thin))
-//
-//                Button(action: {
-//
-//                }) { Section {
-//                    Text("Create Budget Trip")
-//                        .foregroundColor(Color.white)
-//                        .padding(.horizontal)
-//                        .padding(.vertical, 15.0)
-//                        .frame(minWidth:0,  maxWidth: .infinity, alignment: .top)
-//                        .background(
-//                            RoundedRectangle(cornerRadius: 10)
-//                                .stroke(lineWidth: 0)
-//                                .background(Color("CustomColor").cornerRadius(10)))
-//                }
-//                .frame(minWidth:0, maxWidth: 200, alignment: .top)
-//                }
+//            }, label: {
+//                Image(systemName: "plus")
+//                    .font(.system(size: 30))
 //            }
+////           ) .navigationBarItems(
+////
+////                trailing:
+////                    NavigationLink(
+////                        destination: AddExpensesPage,
+////                        label: {
+////                            Image(systemName: "plus")
+////                        })
+//
+//            )
+//            .accentColor(Color("CustomColor"))
+//                }
+//
 //
 //        }
 //
-//    }
-        NavigationView {
-            TabView(selection: .constant(1)) {
-                Text("INI SATU").tabItem {
-                    Image(systemName: "house")
-                    Text("Home") }.tag(1)
-                
-                Text("INI DUA").tabItem {
-                    Image(systemName: "airplane.circle")
-                    Text("My Trips") }.tag(2)
-            }
-            
-            .navigationTitle("Budget Trip")
-            
-            Button (action: {
-                
-            }, label: {
-                Image(systemName: "plus")
-                    .font(.system(size: 30))
-            }
-//           ) .navigationBarItems(
 //
-//                trailing:
-//                    NavigationLink(
-//                        destination: AddExpensesPage,
-//                        label: {
-//                            Image(systemName: "plus")
-//                        })
-                    
-            )
-            .accentColor(Color("CustomColor"))
-                }
-            
-
-        }
-
-
-struct AddExpensesPage: View {
-    
-    @Environment(\.presentationMode) var presentationMode
-    
-    var body: some View {
-        ZStack {
-            Color.white.ignoresSafeArea()
-                .navigationTitle("New Trip")
-            
-            VStack {
-                Button("Back Button") {
-                    presentationMode.wrappedValue.dismiss()
-                }
-                
-            }
-        }
-        
-        
-    }
+//struct AddExpensesPage: View {
+//
+//    @Environment(\.presentationMode) var presentationMode
+//
+//    var body: some View {
+//        ZStack {
+//            Color.white.ignoresSafeArea()
+//                .navigationTitle("New Trip")
+//
+//            VStack {
+//                Button("Back Button") {
+//                    presentationMode.wrappedValue.dismiss()
+//                }
+//
+//            }
+//        }
+//
+//
+//    }
 }
 }
     
--- Travoila/View/NewTripView.swift
+++ Travoila/View/NewTripView.swift
@@ -8,13 +8,74 @@
 import SwiftUI
 
 struct Newtrip: View {
+    
+    @State var tripTitle: String = ""
+    @State var tripDestination: String = ""
+    
+    @State var startDate: Date = Date()
+    @State var endDate: Date = Date()
+    
+    @State var totalBudgetEstimation: String = ""
+    
+    @Binding var trips: [Trip]
+    @Binding var isNoTrip: Bool
+    
+    @Environment(\.presentationMode) var mode: Binding<PresentationMode>
+
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Form {
+                Section ( header: Text("Trip Title")) {
+                    TextField("Trip Title", text: $tripTitle)
+                }
+
+                Section ( header: Text("Trip Destination")) {
+                    TextField("Trip Destination", text: $tripDestination)
+                }
+
+                Section(header: Text("Date")){
+                    DatePicker("Start Date", selection: $startDate, displayedComponents: .date)
+                    DatePicker("End Date", selection: $endDate, displayedComponents: .date)
+                }
+
+                Section(header: Text("Total Budget Estimation")){
+                    TextField("IDR 0",text: $totalBudgetEstimation)
+                }
+            }
+
+            Button(action: {
+                createNewTrip()
+            }){
+                Text("Save")
+                    .bold()
+                    .frame(width: 340, height: 50)
+                    .background(Color("CustomColor"))
+                    .foregroundColor(.white)
+                    .cornerRadius(10.0)
+            }
+            .frame(minWidth:0,  maxWidth: .infinity)
+        }
+        .background(Color.gray.opacity(0.1))
+    }
+    
+    func createNewTrip(){
+        trips.append(
+            Trip(title: tripTitle, destination: tripDestination, startDate: startDate, endDate: endDate, totalBudgetEstimation: Int(totalBudgetEstimation) ?? 0 )
+        )
+        
+        isNoTrip = false
+        
+        mode.wrappedValue.dismiss()
     }
 }
 
 struct Newtrip_Previews: PreviewProvider {
+    
+    @State private static var dummyData: [Trip] = []
+    @State private static var isNoTrip: Bool = true
+    
     static var previews: some View {
-        Newtrip()
+        Newtrip(trips: $dummyData, isNoTrip: $isNoTrip)
     }
 }
