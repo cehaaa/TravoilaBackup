a93f7c902a723cd2e939746eaaaa6ffc0e1df6d7
Patrick Louis <patrick.louis898@gmail.com>
2022-05-18 20:58:30 +0700

    Home Screen #3

---
+++ Travoila/View/NewTripView.swift
@@ -0,0 +1,20 @@
+//
+//  NewTripView.swift
+//  Travoila
+//
+//  Created by Patrick Louis on 18/05/22.
+//
+
+import SwiftUI
+
+struct NewTripView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewTripView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewTripView()
+    }
+}
